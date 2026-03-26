4bc3b9a3a1936f9ac3783d50cd319623232ffabb
0507spc <0507spc@gmail.com>
2026-03-26 22:26:41 +0000

    Add fs module import to server.js

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { createCanvas } = require('canvas');
 const dayjs = require('dayjs');
 const cors = require('cors');
+const fs = require('fs');
+
 
 const app = express();
 app.use(cors());
